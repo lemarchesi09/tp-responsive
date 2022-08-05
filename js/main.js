381abf43263bd206d2f10f3f83cd59d976fef643
Enzo Marchesi <80432840+lemarchesi09@users.noreply.github.com>
2022-08-05 09:34:59 -0300

    :sparkles: Fix: navbar modo oscuro

--- js/main.js
+++ js/main.js
@@ -8,30 +8,33 @@ let menuCont = document.querySelector('.menu-open-close')
 let menuLista = document.querySelector('.menu-lista')
 
 let btnModoOscuro = document.querySelector('.boton-mo')
+let imgLunaNegra = document.querySelector('.luna-negra')
 
 let navBar = document.querySelector('nav')
 
 let linksNavBar = document.querySelectorAll('.link-a')
+
+let imgServices = document.querySelectorAll('.grid-tarj img')
 
 let seccAmarillas = document.querySelectorAll('.fon-amarillo') 
 let secBlancas = document.querySelectorAll('.fon-blanco')
 
 let icoTwiteer = document.querySelector('.ico-twitter')
 
-botonMenuOpen.addEventListener('click', e =>{
+botonMenuOpen.addEventListener('click', () =>{
     menuCont.style.display = 'block'
     botonMenuOpen.style.display = 'none'
     botonMenuClose.style.display = 'block'
     menuLista.style.display = 'flex'
 })
 
-botonMenuClose.addEventListener('click', e =>{
+botonMenuClose.addEventListener('click', () =>{
     botonMenuOpen.style.display = 'block'
     menuCont.style.display = 'none'
     botonMenuClose.style.display = 'none'
 })
 
-btnModoOscuro.addEventListener('click', e =>{
+btnModoOscuro.addEventListener('click', () =>{
     navBar.classList.toggle('nav-osc');
 
     secBlancas.forEach((item)=>{
@@ -40,9 +43,17 @@ btnModoOscuro.addEventListener('click', e =>{
 
     //se arregla con item.style pero no puedo volver al color original
     linksNavBar.forEach((item) =>{
-        item.classList.toggle('.link-mod-oscuro');
+        // item.classList.toggle('link-mod-oscuro')
+        // item.classList.toggle('.link-mod-oscuro');
     })
-    
+
+    imgServices.forEach((item) =>{
+        item.classList.toggle('img-services-oscuro')
+    })
+
+    imgLunaNegra.classList.toggle('luna-blanca')
+
+
 
 /*     seccAmarillas.classList.toggle('fondo-negro') */
     
